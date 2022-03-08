05e77092bceb056291fd4a0c2140ac7df1b2329b
Yersel Hurtado <yersel500@gmail.com>
2022-03-08 10:06:26 -0500

    Fix display home

--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
 import './main.scss';
 const button = document.querySelector('.button');
 const container = document.querySelector('.container');
+const container2 = document.querySelector('.container2');
 
 button.addEventListener('click', loadPokemons);
 
+window.onload = () => {
+  loadPokemons();
+};
+
 async function loadPokemons() {
-  try {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon/25');
-    const data = await response.json();
-    showPokemons(data.sprites.front_default);
-    console.log(data);
+  for ( let i=1; i <= 36; i += 1) {
+    // try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+      const data = await response.json();
+      showPokemons(data);
+      console.log(data);
+    // }
+    // catch(error) {
+    //   console.log(error);
+    // }
   }
-  catch(error) {
-    console.log(error);
-  }
+
 }
 
 function showPokemons(info) {
-  const card = document.createElement('div');
-  card.innerHTML = `<img src=${info} alt="Pikachu">`;
-  container.appendChild(card);
+ 
+  container.innerHTML += `
+  <div class="col">
+    <div class="card h-100">
+      <img src=${info.sprites.front_default} class="card-img-top" alt=${info.name}>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  `;
 }
 
 // importar app.js
