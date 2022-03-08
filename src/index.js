5d6b4f1c47a3f1116598975b66a09e81c60dd9b2
Yersel Hurtado <yersel500@gmail.com>
2022-03-07 23:31:18 -0500

    get data from api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './main.scss';
+const button = document.querySelector('.button');
+const container = document.querySelector('.container');
+
+button.addEventListener('click', loadPokemons);
+
+async function loadPokemons() {
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/25');
+    const data = await response.json();
+    showPokemons(data.sprites.front_default);
+    console.log(data);
+  }
+  catch(error) {
+    console.log(error);
+  }
+}
+
+function showPokemons(info) {
+  const card = document.createElement('div');
+  card.innerHTML = `<img src=${info} alt="Pikachu">`;
+  container.appendChild(card);
+}
 
 // importar app.js
